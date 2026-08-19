f7e99ded471b376080653187ab5465c21050724d
luherramientas-create <luherramientas@gmail.com>
2026-08-18 21:18:51 -0600

    FASE 7.6-B: activar caché final de consolidación

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='memoria-lector-v7-6-0';
+const CACHE='memoria-lector-v7-6-1';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./map-labels.js','./backup.js','./duplicate-manager.js','./character-name-guard.js','./relationship-guard.js','./delete-manager.js','./session-manager.js','./cover-manager.js','./mobile-map-fullscreen.js','./neural-map-real.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('memoria-lector-')&&key!==CACHE).map(key=>caches.delete(key))))));
